2e09cb26c74572fea439afd2a416e396a891d0a8
Gianluigi Tiesi <sherpya@netfarm.it>
2019-08-30 05:41:45 +0200

    fixed custom stdbool include

--- include/stdbool.h
+++ include/stdbool.h
@@ -1,10 +1,18 @@
 #ifndef _STDBOOL_H_
 #define _STDBOOL_H_
 
+#ifndef __cplusplus
+
 #if defined(_MSC_VER) && (_MSC_VER < 1800)
 typedef enum { _Bool_must_promote_to_int = -1, false = 0, true = 1 } _Bool;
 #endif
 
 #define bool _Bool
+#define false   0
+#define true    1
+
+#endif /* __cplusplus */
+
+#define __bool_true_false_are_defined 1
 
 #endif /* _STDBOOL_H_ */
