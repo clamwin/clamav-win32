91f843d720f6e46e781e3f4124ce4b2f340805cc
sherpya <sherpya@netfarm.it>
2019-10-06 20:38:05 +0200

    cmake: source filters for visual studio projects

--- cmake/libclamav.cmake
+++ cmake/libclamav.cmake
@@ -1,27 +1,3 @@
-file(GLOB libclamav_srcs
-    ${CLAMAV}/libclamav/*.c
-    ${CLAMAV}/libclamav/7z/*.c
-    ${CLAMAV}/libclamav/lzw/*.c
-    ${CLAMAV}/libclamav/nsis/*.c
-    ${CLAMAV}/libclamav/regex/*.c
-    ${CLAMAV}/libclamav/tomsfastmath/*/*.c
-    ${CLAMAV}/libclamav/jsparse/js-norm.c)
-
-list(REMOVE_ITEM libclamav_srcs
-    ${CLAMAV}/libclamav/others.c
-    ${CLAMAV}/libclamav/regex/engine.c
-    ${CLAMAV}/libclamav/bytecode_nojit.c
-    ${CLAMAV}/libclamav/tomsfastmath/misc/fp_ident.c)
-
-file(GLOB libclamav_dllmain ${CMAKE_SOURCE_DIR}/src/dllmain/*.c)
-list(APPEND libclamav_srcs ${libclamav_dllmain})
-
-if(MINGW)
-    list(APPEND libclamav_srcs ${CMAKE_SOURCE_DIR}/src/dllmain/pthread-mingw.c)
-else()
-    list(APPEND libclamav_srcs ${3RDPARTY}/pthreads/pthread.c)
-endif()
-
 if(MINGW)
     set(OPENSSL_LIBRARY_PATH ${3RDPARTY}/openssl/lib/mingw32)
 elseif(CMAKE_CL_64)
@@ -40,8 +16,37 @@ find_library(OPENSSL_CRYPTO_LIBRARY
     HINTS ${OPENSSL_LIBRARY_PATH}
 )
 
+file(GLOB libclamav_srcs
+    ${CLAMAV}/libclamav/*.c
+    ${CLAMAV}/libclamav/7z/*.c
+    ${CLAMAV}/libclamav/lzw/*.c
+    ${CLAMAV}/libclamav/nsis/*.c
+    ${CLAMAV}/libclamav/regex/*.c
+    ${CLAMAV}/libclamav/tomsfastmath/*/*.c
+    ${CLAMAV}/libclamav/jsparse/js-norm.c)
+list(REMOVE_ITEM libclamav_srcs
+    ${CLAMAV}/libclamav/others.c
+    ${CLAMAV}/libclamav/regex/engine.c
+    ${CLAMAV}/libclamav/bytecode_nojit.c
+    ${CLAMAV}/libclamav/tomsfastmath/misc/fp_ident.c)
+
+file(GLOB libclamav_win32 ${CMAKE_SOURCE_DIR}/src/dllmain/*.c)
+if(MINGW)
+    list(APPEND libclamav_win32 ${CMAKE_SOURCE_DIR}/src/dllmain/pthread-mingw.c)
+else()
+    list(APPEND libclamav_win32 ${3RDPARTY}/pthreads/pthread.c)
+endif()
+
+file(GLOB_RECURSE libclamav_win32_headers ${CMAKE_SOURCE_DIR}/include/*.h)
+
+source_group("Header Files" FILES ${libclamav_win32_headers})
+source_group("Source Files" FILES ${libclamav_srcs})
+source_group("Win32 Files" FILES ${libclamav_win32})
+
 add_library(libclamav SHARED
+    ${libclamav_win32_headers}
     ${libclamav_srcs}
+    ${libclamav_win32}
     ${CMAKE_SOURCE_DIR}/resources/libclamav.rc
     ${CMAKE_SOURCE_DIR}/libclamav.def)
 
