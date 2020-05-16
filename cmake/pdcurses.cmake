6d976d5be3c5b218194e48f190ed998201584828
Gianluigi Tiesi <sherpya@netfarm.it>
2020-05-16 02:21:49 +0200

    PDCurses: build for NT4 on mingw 32bit

--- cmake/pdcurses.cmake
+++ cmake/pdcurses.cmake
@@ -15,5 +15,9 @@ set(pdc_src_files
 
 add_library(PDCurses STATIC ${pdc_src_files} ${pdcurses_src_files})
 target_include_directories(PDCurses PRIVATE ${PDCURSES})
+if((MINGW) AND (CLAMAV_ARCH STREQUAL "x86"))
+    # 0x0400 is _WIN32_WINNT_NT4
+    target_compile_definitions(PDCurses PRIVATE WINVER=0x0400)
+endif()
 
 install(FILES ${PDCURSES}/README.md DESTINATION ${CMAKE_INSTALL_PREFIX}/copyright RENAME PDCurses-README.md)
