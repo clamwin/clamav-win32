c02ad649c6ffec07b6e6c00e763e3b0b21e98337
Gianluigi Tiesi <sherpya@netfarm.it>
2022-06-16 20:00:20 +0200

    fixed json-c compilation on mingw cross build

--- cmake/json-c.cmake
+++ cmake/json-c.cmake
@@ -2,7 +2,7 @@ file(GLOB_RECURSE json-c_headers ${CLAMWIN_DIR}/include/json-c/*.h)
 file(GLOB json-c_sources ${3RDPARTY_DIR}/json-c/*.c)
 
 add_library(json-c STATIC ${json-c_headers} ${json-c_sources})
-target_include_directories(json-c PRIVATE ${CLAMWIN_DIR}/include/json-c ${CLAMWIN_DIR}/include)
+target_include_directories(json-c PRIVATE ${CLAMWIN_DIR}/include/json-c ${CLAMWIN_DIR}/include ${3RDPARTY_DIR}/json-c)
 target_compile_definitions(json-c PRIVATE HAVE_CONFIG_H)
 target_compile_options(json-c PRIVATE $<$<C_COMPILER_ID:MSVC>:/wd4244 /wd4267>)
 
