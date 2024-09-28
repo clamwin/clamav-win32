3def0e1a3e36c7b7dd5c4d22c85df47daa8b34c6
Gianluigi Tiesi <sherpya@gmail.com>
2024-09-28 14:06:04 +0200

    added BCryptGenRandom equivalent on mingw

---
+++ src/dllmain/bcrypt.c
@@ -0,0 +1,22 @@
+#if defined(__MINGW32__) && !defined(__MINGW64__)
+#include <windows.h>
+#include <ntstatus.h>
+#include <wincrypt.h>
+#include <bcrypt.h>
+
+NTSTATUS WINAPI BCryptGenRandom(BCRYPT_ALG_HANDLE hAlgorithm, PUCHAR pbBuffer, ULONG cbBuffer, ULONG dwFlags) {
+    if (hAlgorithm || !(dwFlags & BCRYPT_USE_SYSTEM_PREFERRED_RNG))
+        return STATUS_NOT_IMPLEMENTED;
+
+    HCRYPTPROV hProvider;
+    if (CryptAcquireContext(&hProvider, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
+    {
+        BOOL ret = CryptGenRandom(hProvider, cbBuffer, pbBuffer);
+        CryptReleaseContext(hProvider, 0);
+        if (ret)
+           return STATUS_SUCCESS;
+    }
+
+    return STATUS_INVALID_PARAMETER;
+}
+#endif
