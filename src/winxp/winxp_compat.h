1d9d579e2ff3fdfe655b536399c3490fccddd457
Gianluigi Tiesi <sherpya@gmail.com>
2025-03-05 04:11:26 +0100

    winxp: trace hardcoded

--- src/winxp/winxp_compat.h
+++ src/winxp/winxp_compat.h
@@ -12,7 +12,7 @@
 #include <wchar.h>
 #include <strsafe.h>
 
-#if 1// def TRACE_COMPAT
+#ifdef TRACE_COMPAT
 #include <stdio.h>
 #define TRACE(format, ...) fwprintf(stderr, L"[winxp] " format, ##__VA_ARGS__)
 #else
