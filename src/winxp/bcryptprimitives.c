ec63b194e1adb81a7788055cde8e3211bb87c44e
Gianluigi Tiesi <sherpya@gmail.com>
2025-03-06 23:52:45 +0100

    fmt

--- src/winxp/bcryptprimitives.c
+++ src/winxp/bcryptprimitives.c
@@ -36,9 +36,7 @@ WINBASEAPI BOOL WINAPI ProcessPrng(void *buffer, size_t size)
     // Acquire a cryptographic context. The CRYPT_VERIFYCONTEXT flag indicates that
     // no persistent key container is needed (suitable for generating random data).
     if (!CryptAcquireContextW(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
-    {
         return FALSE;
-    }
 
     // Generate random bytes and fill the buffer.
     result = CryptGenRandom(hProv, (DWORD)size, (BYTE *)buffer);
