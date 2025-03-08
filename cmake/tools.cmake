858086459a0034286a4ff3a5a96fda0ed3f10bfb
Gianluigi Tiesi <sherpya@gmail.com>
2025-03-08 23:30:16 +0100

    cmake: fixed tools options on msvc

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -8,8 +8,7 @@ add_executable(exeScanner
 target_link_libraries(exeScanner PRIVATE libclamav_common libclamav ws2_32)
 target_include_directories(exeScanner PRIVATE ${CLAMWIN_INCLUDES})
 target_compile_definitions(exeScanner PRIVATE ${CLAMWIN_DEFINES} ${TOOLS_DEFINES})
-target_compile_options(exeScanner PRIVATE -Wall)
-
+target_compile_options(exeScanner PRIVATE $<$<CXX_COMPILER_ID:GNU>:-Wall>)
 
 add_executable(sigcheck
     ${CLAMWIN_DIR}/tools/sigcheck_app.c
@@ -19,7 +18,7 @@ add_executable(sigcheck
 target_link_libraries(sigcheck PRIVATE libclamav_common libclamav ws2_32)
 target_include_directories(sigcheck PRIVATE ${CLAMWIN_INCLUDES})
 target_compile_definitions(sigcheck PRIVATE ${CLAMWIN_DEFINES} ${TOOLS_DEFINES})
-target_compile_options(sigcheck PRIVATE -Wall)
+target_compile_options(sigcheck PRIVATE $<$<CXX_COMPILER_ID:GNU>:-Wall>)
 
 
 add_executable(unscrambler
@@ -27,6 +26,6 @@ add_executable(unscrambler
     ${CLAMWIN_DIR}/tools/unscrambler.rc
 )
 
-target_link_options(unscrambler PRIVATE -municode)
+target_link_options(unscrambler PRIVATE $<$<CXX_COMPILER_ID:GNU>:-municode>)
 target_compile_definitions(unscrambler PRIVATE UNICODE)
-target_compile_options(unscrambler PRIVATE -Wall)
+target_compile_options(unscrambler PRIVATE $<$<CXX_COMPILER_ID:GNU>:-Wall>)
