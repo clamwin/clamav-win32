d386988013973674adc9f11f87297d48613469b6
Gianluigi Tiesi <sherpya@gmail.com>
2025-03-18 19:41:19 +0100

    cmake: don't install rust archive

--- cmake/legacy.cmake
+++ cmake/legacy.cmake
@@ -101,9 +101,8 @@ add_custom_command(
     DEPENDS "${RUST_ARCHIVE}"
 )
 
-add_custom_target(filter_clamav_rust ALL DEPENDS "${RUST_FILTERED_ARCHIVE}")
+add_custom_target(filter_clamav_rust DEPENDS "${RUST_FILTERED_ARCHIVE}")
 set_target_properties(clamav_rust PROPERTIES IMPORTED_LOCATION "${RUST_FILTERED_ARCHIVE}")
 add_dependencies(libclamav filter_clamav_rust)
-add_dependencies(filter_clamav_rust clamav_rust)
 
 target_link_options(libclamav PRIVATE $<$<C_COMPILER_ID:MSVC>:/FORCE:MULTIPLE>)
